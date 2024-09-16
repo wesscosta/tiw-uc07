fcbbcbe70469d4308a244b819de45aba5d0ab649
Weslley Costa <weslleycosta.ti@gmail.com>
2024-09-16 17:55:26 -0300

    questões 07 e 08

---
+++ Aula10/Questao08/main.py
@@ -0,0 +1,47 @@
+# Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho (estomago) e pelo menos os métodos comer(), verBucho() e digerir(). Faça um programa ou teste interativamente, criando pelo menos dois macacos, alimentando-os com pelo menos 3 alimentos diferentes e verificando o conteúdo do estomago a cada refeição. Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?
+
+from typing import Self
+
+
+class MacacoClass :
+  def __init__(self,nome):
+    self.nome = nome 
+    self.bucho = []
+    
+  def comer(self,comida):
+    self.bucho.append(comida)
+    
+  def verBucho(self):
+    return (self.bucho)
+  
+  def digerir(self):
+    del self.bucho[0]
+    
+    
+  def __str__(self) -> str:
+   return verBucho()
+ 
+class Main :
+  macaco1 = MacacoClass("macaco")
+  macaco1.comer("banana")
+  macaco1.comer("limão")
+  macaco1.comer("lima")
+  
+  macaco2 = MacacoClass("macaco2")
+  macaco2.comer("laranja")
+  macaco2.comer("tanja")
+  macaco2.comer("arroz")
+  
+  print(macaco1.verBucho())
+  print(macaco2.verBucho())
+  
+  macaco2.digerir()
+  print(macaco2.verBucho())
+  macaco1.comer(macaco2)
+  
+  print(macaco1.verBucho())
+  
+    
+    
+    
+  
